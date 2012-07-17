16a2b38831815f3f1404582a8f847fab20bdbadc
Matthew Du Pont <dupont@okcupidlabs.com>
2012-07-16 20:29:25 -0700

    exceptions and triangles v2

--- triangle.rb
+++ triangle.rb
@@ -13,8 +13,14 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+#class TriangleError < StandardError
+#end
+
 def triangle(a, b, c)
 #get a point for each side equal, then do # of points - 3 eq, 1 iso, 0 scal
+    fail TriangleError unless a + b > c && b + c > a && c + a > b && a > 0 && b > 0 && c > 0
+
   counter = 0
   counter += 1 if a == b
   counter += 1 if b == c
